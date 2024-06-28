0d910ad25ecf51e5da5f9227572f92b124049d2d
Pashidy <eldafrancis@yahoo.com>
2024-06-28 20:12:38 +0100

    ES6 Basics Tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    /*eslint-disable no-unused vars*/
+    /*eslint-disable*/
     const task = true;
     const task2 = false;
-    /*eslint-enable no-unused vars*/
   }
 
   return [task, task2];
